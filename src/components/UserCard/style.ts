a3ee3fc59c986a3278878955e9ea1c651cef192a
anavgbc <agomesbc@gmail.com>
2023-02-23 13:47:26 -0300

    feat: About user card

---
+++ src/components/UserCard/style.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const Page = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: gray;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 5px;
+
+  background-color: ${(props) => props.theme.colors.grey10};
+
+  max-width: 46rem;
+  width: 95%;
+  height: 45%;
+
+  @media screen and (min-width: 768px) {
+    width: 40%;
+    height: 55%;
+  }
+
+  @media screen and (min-width: 1024px) {
+    width: 30%;
+  }
+
+  margin: 0 auto;
+`;
+
+export const UserInfo = styled.div`
+  width: 80%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+
+  text-align: justify;
+
+  button {
+    width: 80%;
+  }
+`;
+
+export const UserCardImg = styled.img`
+  border-radius: 50%;
+  max-width: 25%;
+`;
