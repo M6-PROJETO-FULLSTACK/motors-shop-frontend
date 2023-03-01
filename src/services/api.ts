4966c4ff1a8768db8f31bc6e595124ac0563bce9
Giovanna Oliveira <giovannabeathryce.pso@gmail.com>
2023-03-01 12:20:00 -0300

    feat: axios config

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,2 +1,8 @@
-export default {};
-//utilização da biblioteca axios para requisição
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://127.0.0.1:3001/',
+  timeout: 5000,
+});
+
+export default api;
