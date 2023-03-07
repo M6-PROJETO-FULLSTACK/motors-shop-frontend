e610c7fe98333c7fe8e092d5e659047bd582aad6
Giovanna Oliveira <giovannabeathryce.pso@gmail.com>
2023-03-07 15:25:34 -0300

    feat:DB vehicle rendering

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -5,67 +5,101 @@ import VehicleList from "../../components/VehicleList";
 import { ContainerPage } from "../Login/style";
 import { ProductsContainer , VehicleSection} from "./style";
 import AuctionCard from "../../components/AuctionCard";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../../providers/AuthContext";
 import Text from "../../styles/texts";
+import api from "../../services/api";
+
+interface IVehicleProps {
+  title: string;
+  year: string;
+  mileage: string;
+  price: number;
+  description: string;
+  cover: string;
+  userName: string;
+  userId: string;
+  isActive: boolean;
+  id: string;
+  advertiseType:boolean;
+  vehicleType:boolean; 
+}
+
 
 const Profile = () => {
-  const {user} = useContext(AuthContext)
-  const vehicles = user?.vehicles
+  const userId = localStorage.getItem("@MotorsShop:id")
+  const [vehicles, setVehicles] = useState<IVehicleProps[]>([])
+  const cars:IVehicleProps[] = []
+  const motocycle:IVehicleProps[] =[]
 
+  useEffect(()=>{
+    const getVehicles = async () => {
+      try {
+        const { data } = await api.get(`/vehicles/user/${userId}`);
+        setVehicles(data)
+      } catch (error) {
+        console.error(error);
+				}
+      };
+      getVehicles();
 
-  const cards = [
-    {
-      title: "Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200",
-      year: "2019",
-      mileage: "0",
-      price: 110000,
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      cover:
-        "https://i.pinimg.com/originals/24/13/73/241373ff7d3ad9202834eb7428ba4750.jpg",
-      userName: "Nome de Teste Realmente Muito Grande",
-      userId: "1",
-      isActive: true,
-      id: "1",
-    },
-  ];
+  },[])
 
-  for (let i = 0; i < 8; i++) {
-    cards.push(cards[0]);
-  }
+  vehicles.map((vehicle:IVehicleProps) =>{
+    if(vehicle.vehicleType === true){
+      cars.push(vehicle)
+    }else if(vehicle.vehicleType === false){
+      motocycle.push(vehicle)
+    }
+  })
+
   return (
     <>
       <Navbar />
       <ContainerPage>
         <UserCard profile />
         <ProductsContainer>
-          {/* {
+          {
             vehicles!.length === 0 ?
-            <VehicleSection>
               <Text className="heading1 text" weight={600}>Você ainda não possui nenhum veículo cadastrado</Text>
-            </VehicleSection>
             :
-            ( */}
+            
             <>
               <VehicleSection id="leilao">
-              <AuctionCard 
-                img="https://cdn-icons-png.flaticon.com/512/21/21104.png"
-                name="Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200 "
-                description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem..."
-                km="0"
-                owner="Rodrigo Tavares"
-                price="110000"
-                year="2019"
-                />
+                  <AuctionCard 
+                    img="https://cdn-icons-png.flaticon.com/512/21/21104.png"
+                    name="Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200 "
+                    description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem..."
+                    km="0"
+                    owner="Rodrigo Tavares"
+                    price="110000"
+                    year="2019"
+                    />
+                  <AuctionCard 
+                    img="https://cdn-icons-png.flaticon.com/512/21/21104.png"
+                    name="Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200 "
+                    description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem..."
+                    km="0"
+                    owner="Rodrigo Tavares"
+                    price="110000"
+                    year="2019"
+                    />
+                  <AuctionCard 
+                    img="https://cdn-icons-png.flaticon.com/512/21/21104.png"
+                    name="Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200 "
+                    description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem..."
+                    km="0"
+                    owner="Rodrigo Tavares"
+                    price="110000"
+                    year="2019"
+                    />
               </VehicleSection>
-              <VehicleList list={cards} id="carros" />
-              <VehicleList list={cards} id="motos" />
+              <VehicleList list={cars} id="carros" />
+              <VehicleList list={motocycle} id="motos" />
           </>
-           {/* )
-          } */}
+            }
         </ProductsContainer>
-      </ContainerPage>''
+      </ContainerPage>
       <Footer />
     </>
   );
